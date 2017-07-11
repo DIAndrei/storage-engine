2e149402eb49b3e294edab5aea8476d78751c2ab
Andrei Diaconu <andrei_di@yahoo.com>
2017-07-11 14:54:06 +0300

    Moved StorageEngine in a separate file

--- index.js
+++ index.js
@@ -1,72 +1,5 @@
-'use strict';
+const StorageEngine = require('./lib/StorageEngine.js');
 
-const fs = require('fs');
-
-function StorageEngine(filePath) {
-    this.myState = {};
-    this.filePath = filePath;
-}
-
-StorageEngine.prototype.set = function (myKey, myValue, persistent) {
-    Object.defineProperty(this.myState, myKey, {
-        value: myValue,
-        enumerable: persistent,
-        writable: true,
-        configurable: true
-    });
-}
-
-StorageEngine.prototype.get = function (myKey) {
-    return this.myState[myKey];
-}
-
-StorageEngine.prototype.load = function (callback) {
-    fs.readFile(this.filePath, { encoding: 'utf-8' }, this.loadFromString.bind(this));
-}
-
-StorageEngine.prototype.loadFromString = function (err, content) {
-    if (err) {
-        return myCallback(err);
-    }
-    if (content) {
-        this.myState = JSON.parse(content);
-    }
-    else {
-        console.trace('Invalid or no file content');
-        return;
-    }
-}
-
-StorageEngine.prototype.save = function (callback) {
-    var inMemory = this.myState,
-        inFile,
-        self = this;
-
-    fs.readFile(this.filePath, { encoding: 'utf-8' }, function (err, content) {
-        if (content) {
-            inFile = JSON.parse(content);
-        }
-        else {
-            console.trace('Invalid or no file content');
-            return;
-        }
-
-        for (var i in inMemory) {
-            inFile[i] = inMemory[i];
-        }
-
-        // console.log(inMemory, inFile);
-
-        fs.writeFile(self.filePath, JSON.stringify(inFile), function (err) {
-            if (err) {
-                return callback(err);
-            }
-            callback();
-        });
-
-    });
-
-}
 
 function myCallback(err) {
     if (err) {
@@ -74,7 +7,7 @@ function myCallback(err) {
     }
 }
 
-var storageEngine1 = new StorageEngine('./store/store.txt');
+let storageEngine1 = new StorageEngine('./store/store.txt');
 
 storageEngine1.set('nume', 'Gheorghe', true);
 storageEngine1.set('adresa', 'Strada', false);
@@ -86,12 +19,10 @@ storageEngine1.set('test', 'QPWOEQKIWPQIPIP', true);
 storageEngine1.set('adresa', 'str. Copacului', true);
 storageEngine1.set('cheiecarenuexista', 'laskdj', true);
 storageEngine1.set('asd', 'asd', false);
+
 // storageEngine1.load(myCallback);
 
+// storageEngine1.set('nume', 'Gigi', true);
+// storageEngine1.set('test3', 'pqowiepqwoeipqwoeipqowei', true);
+
 storageEngine1.save(myCallback);
-
-
-
-// setTimeout(function () {
-//     console.log(storageEngine1.get('email'));
-// }, 1000);
